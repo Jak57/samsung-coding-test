38aa521004006e95c400f280564be3ece63d9cd7
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-17 21:36:41 +0600

    Create p9. Wormhole.cpp

---
+++ p9. Wormhole.cpp
@@ -0,0 +1,54 @@
+// https://github.com/s-kachroo/SamsungPractice/blob/master/wormhole%20DFS.cpp
+// https://www.hackerrank.com/contests/target-samsung-13-nov19/challenges/warmholes
+
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+
+int n, result, temp;
+int w[100][5];
+int mask[100];
+
+int dist(int sx, int sy, int tx, int ty) {
+    return (abs(sx-tx) + abs(sy-ty));
+}
+
+void wormhole(int sx, int sy, int tx, int ty, int cost) {
+    result = min(result, dist(sx, sy, tx, ty) + cost);
+
+    int i;
+    for (i = 0; i < n; i++) {
+        if (mask[i] == 0) {
+            mask[i] = 1;
+            // lower end
+            temp = dist(sx, sy, w[i][0], w[i][1]) + w[i][4] + cost;
+            wormhole(w[i][2], w[i][3], tx, ty, temp);
+
+            // upper end
+            temp = dist(sx, sy, w[i][2], w[i][3]) + w[i][4] + cost;
+            wormhole(w[i][0], w[i][1], tx, ty, temp);
+            mask[i] = 0;
+        }
+    }
+}
+
+int main() {
+    int t, i, j;
+    int sx, sy, tx, ty;
+    cin>> t;
+    while (t--) {
+        cin>> n;
+        cin>> sx >> sy >> tx >> ty;
+        for (i = 0; i < n; i++) {
+            mask[i] = 0;
+            for (j = 0; j < 5; j++)
+                cin>> w[i][j];
+        }
+
+        result = INT_MAX;
+        wormhole(sx, sy, tx, ty, 0);
+        cout<< result << "\n";
+    }
+    return 0;
+}
