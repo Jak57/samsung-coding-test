d6b4dbca80ed02d821c1c5395dea9db0a633a61b
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-22 22:35:17 +0600

    Update p5. Mr Lee.cpp

--- p5. Mr Lee.cpp
+++ p5. Mr Lee.cpp
@@ -49,3 +49,55 @@ int main() {
 
     return 0;
 }
+
+
+// 2
+
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+
+int n;
+int result;
+
+void dfs(int graph[][100], int *visited, int src, int cost, int count) {
+    if (count == n) {
+        if (graph[src][0] != 0)
+            result = min(result, cost + graph[src][0]);
+        return;
+    }
+
+    int i;
+    for (i = 0; i < n; i++) {
+        if (graph[src][i] != 0 && visited[i] == 0) {
+            visited[i] = 1;
+            dfs(graph, visited, i, cost + graph[src][i], count+1);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main() {
+    int graph[100][100], visited[100];
+    int i, j, t;
+    cin>> t;
+
+    while (t--) {
+        cin>> n;
+        for (i = 0; i < n; i++) {
+            visited[i] = 0;
+            for (j = 0; j < n; j++) 
+                cin>> graph[i][j];
+        }
+
+        result = INT_MAX;
+        visited[0] = 1;
+        int src = 0, count = 1, cost = 0;
+        dfs(graph, visited, src, cost, count);
+        cout<< result << "\n";
+    }
+
+    return 0;
+}
+
