c75f2b8e514e6693b8433db9a39f20e9da6ddfd9
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-22 13:47:15 +0600

    Update p3. Burst Balloon.cpp

--- p3. Burst Balloon.cpp
+++ p3. Burst Balloon.cpp
@@ -102,3 +102,60 @@ int main() {
     return 0;
 }
 
+3.
+
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+
+int main() {
+    int n, i, j, len;
+    int k, leftVal, rightVal, before, after;
+    int dp[100][100];
+    int arr[100];
+    cin>> n;
+
+    for (i = 0; i < n; i++)
+        cin>> arr[i];
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            dp[i][j] = 0;
+    }
+
+    for (len = 1; len <= n; len++) {
+        for (i = 0; i <= n-len; i++) {
+            j = i + len - 1;
+
+            for (k = i; k <= j; k++) {
+                leftVal = 1;
+                rightVal = 1;
+                before = 0;
+                after = 0;
+
+                if (i != 0) {
+                    leftVal = arr[i-1];
+                } 
+
+                if (j != n-1) {
+                    rightVal = arr[j+1];
+                } 
+
+                if (i != k)
+                    before = dp[i][k-1];
+                if (j != k)
+                    after = dp[k+1][j];
+
+                if (leftVal == 1 && rightVal == 1)
+                    dp[i][j] = max(dp[i][j], before + leftVal * arr[k] * rightVal + after);
+                else
+                    dp[i][j] = max(dp[i][j], before + leftVal * 1 * rightVal + after);
+            }
+        }
+    }
+    cout<< dp[0][n-1] << "\n";
+
+    return 0;
+}
+
