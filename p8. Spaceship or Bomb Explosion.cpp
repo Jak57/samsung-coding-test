85d714cb01771218a7e698520d4814544f9f8619
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2024-01-23 13:52:35 +0600

    Update p8. Spaceship or Bomb Explosion.cpp

--- p8. Spaceship or Bomb Explosion.cpp
+++ p8. Spaceship or Bomb Explosion.cpp
@@ -67,3 +67,74 @@ int main() {
 }
 
 
+// 2
+
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<climits>
+using namespace std;
+
+int n, m = 5;
+int matrix[100][100];
+
+bool inLimits(int r, int c) {
+    return ((r >= 0 && r < n) && (c >= 0 && c < m));
+}
+
+void updateMatrix(int r) {
+    r = max(0, r);
+    int i, j;
+    for (i = r; i > r-5; i--) {
+        for (j = 0; j < 5; j++) {
+            if (matrix[i][j] == 2)
+                matrix[i][j] = 0;
+        }
+    }
+}
+
+int maxPoints(int matrix[][100], int r, int c, int bombs) {
+    if (r <= 0 || c < 0 || c >= 5)
+        return 0;
+
+    int ans = 0;
+    // up
+    if (inLimits(r-1, c) && matrix[r-1][c] != 2) {
+        ans = max(ans, matrix[r-1][c] + maxPoints(matrix, r-1, c, bombs));
+    }
+
+    // up-left
+    if (inLimits(r-1, c-1) && matrix[r-1][c-1] != 2) {
+        ans = max(ans, matrix[r-1][c-1] + maxPoints(matrix, r-1, c-1, bombs));
+    }
+
+    if (inLimits(r-1, c+1) && matrix[r-1][c+1] != 2) {
+        ans = max(ans, matrix[r-1][c+1] + maxPoints(matrix, r-1, c+1, bombs));
+    }
+
+    // enemy
+    if (ans == 0 && bombs > 0) {
+        updateMatrix(r-1);
+        ans = maxPoints(matrix, r, c, bombs-1);
+    }
+    return ans;
+}
+
+int main() {
+    int t, c, i, j;
+    cin>> t;
+
+    for (c = 1; c <= t; c++) {
+        cin>> n;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++)
+                cin>> matrix[i][j];
+        }
+
+        int row = n, col = 2, bombs = 1;
+        int ans = maxPoints(matrix, row, col, bombs);
+        printf("#%d %d\n", c, ans);
+    }
+
+    return 0;
+}
